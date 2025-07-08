1ab1db0e3153feafe60a5a4582da95f144787053
Leonel <leo99.fernandes@gmail.com>
2025-07-08 10:06:17 +0000

    Avances

--- APL2/ejercicio4/cliente.c
+++ APL2/ejercicio4/cliente.c
@@ -1,10 +1,10 @@
 
 /*
-Integrantes del grupo : 
--Berti Rodrigo 
-- Burnowicz Alejo 
-- Fernandes Leonel 
-- Federico Agustin 
+Integrantes del grupo :
+-Berti Rodrigo
+- Burnowicz Alejo
+- Fernandes Leonel
+- Federico Agustin
 */
 #include <stdio.h>
 #include <string.h>
@@ -15,27 +15,30 @@ Integrantes del grupo :
 #include <semaphore.h>
 #include <sys/mman.h>
 #include <fcntl.h>
+#include <errno.h>
 
 #define FALSE 0
 #define TRUE 1
 
-typedef struct {
-	char nickname[30];
-	char ultimaLetra;
-	char palabra[30];
-	char palabraCamuflada[30];
-	int intentos;
-	char estadoPartida[30];
+typedef struct
+{
+    char nickname[30];
+    char ultimaLetra;
+    char palabra[30];
+    char palabraCamuflada[30];
+    int intentos;
+    char estadoPartida[30];
 
 } SharedMemory;
 
-
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
     signal(SIGINT, SIG_IGN);
 
     ////////////////////////////////////////////////////////////////////////////
-    if (argc > 7){
+    if (argc > 7)
+    {
         printf("parametros invalidos\n");
         return 1;
     }
@@ -44,11 +47,14 @@ int main(int argc, char *argv[]){
 
     int i = 0;
     bool b_nickname = FALSE;
-    while (i <= argc){
-        if (argv[i] != NULL){
-            if (strcmp(argv[i], "-help") == 0 || strcmp(argv[i], "-h") == 0){
-                //printf("-puerto o -p para indicar el peurto del servidor de destino\n");
-                //printf("-servidor o -s para indicar la ipv4 del servidor de destino\n");
+    while (i <= argc)
+    {
+        if (argv[i] != NULL)
+        {
+            if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0)
+            {
+                // printf("-puerto o -p para indicar el peurto del servidor de destino\n");
+                // printf("-servidor o -s para indicar la ipv4 del servidor de destino\n");
                 printf("Opciones :\n");
                 printf("--nickname o -n para indicar el nickname del cliente\n");
                 printf("--help o -h para mostrar esta ayuda\n");
@@ -56,7 +62,8 @@ int main(int argc, char *argv[]){
                 return 0;
             }
 
-            if (strcmp(argv[i], "-nickname") == 0 || strcmp(argv[i], "-n") == 0){
+            if (strcmp(argv[i], "-nickname") == 0 || strcmp(argv[i], "-n") == 0)
+            {
                 strcpy(nickName, argv[i + 1]);
                 i++;
                 b_nickname = TRUE;
@@ -66,86 +73,131 @@ int main(int argc, char *argv[]){
         i++;
     }
 
-    if (b_nickname == FALSE){
+    if (b_nickname == FALSE)
+    {
         printf("parametro nickname invalido o faltante\n");
         return 1;
     }
 
-////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
 
-SharedMemory* memoriaCompartida;
+    SharedMemory *memoriaCompartida;
 
-int sharedMemInt = shm_open("SHARED_MEM", O_RDWR, 0600);
+    int sharedMemInt = shm_open("SHARED_MEM", O_RDWR, 0600);
 
-if(sharedMemInt == -1){
-	perror("shm_open"); // Imprime el error
-    if (errno == EACCES) {
-        printf("Error: Permisos insuficientes\n");
-    } else if (errno == ENOENT) {
-        printf("Error: Objeto no encontrado\n");
-    } else if (errno == EEXIST) {
-        printf("Error: El objeto ya existe.\n");
-    } else if (errno == EINVAL){
-        printf("Error: Nombre no válido\n");
-    }else if (errno == EMFILE || errno == ENFILE){
-        printf("Error: Demasiados descriptores o archivos abiertos.\n");
+    if (sharedMemInt == -1)
+    {
+        perror("shm_open"); // Imprime el error
+        if (errno == EACCES)
+        {
+            printf("Error: Permisos insuficientes\n");
+        }
+        else if (errno == ENOENT)
+        {
+            printf("Error: Objeto no encontrado\n");
+            printf("Asegúrate de que el servidor esté corriendo antes de iniciar el cliente.\n");
+            exit(1);
+        }
+        else if (errno == EEXIST)
+        {
+            printf("Error: El objeto ya existe.\n");
+        }
+        else if (errno == EINVAL)
+        {
+            printf("Error: Nombre no válido\n");
+        }
+        else if (errno == EMFILE || errno == ENFILE)
+        {
+            printf("Error: Demasiados descriptores o archivos abiertos.\n");
+        }
+        exit(1);
     }
-	exit(1);
-}
 
-ftruncate(sharedMemInt,sizeof(SharedMemory));
+    ftruncate(sharedMemInt, sizeof(SharedMemory));
 
-memoriaCompartida = (SharedMemory*)mmap(NULL,sizeof(SharedMemory),PROT_WRITE,MAP_SHARED,sharedMemInt,0);
+    memoriaCompartida = (SharedMemory *)mmap(NULL, sizeof(SharedMemory), PROT_WRITE, MAP_SHARED, sharedMemInt, 0);
 
-strcpy(memoriaCompartida->nickname,nickName);
+    strcpy(memoriaCompartida->nickname, nickName);
 
-printf("nickname en la mem compartyda %s\n", memoriaCompartida->nickname);
+    printf("nickname en la mem compartyda %s\n", memoriaCompartida->nickname);
 
-close(sharedMemInt);
+    close(sharedMemInt);
 
-sem_t* cliente = sem_open("CLIENTE",O_RDWR,0600,0);
+    sem_t *cliente = sem_open("CLIENTE", O_RDWR, 0600, 0);
 
-if(cliente == SEM_FAILED){
+    if (cliente == SEM_FAILED)
+    {
         printf("error abriendo sem cliente\n");
         exit(1);
-}
+    }
 
-sem_t* servidor = sem_open("SERVIDOR",O_RDWR,0600,0);
+    sem_t *servidor = sem_open("SERVIDOR", O_RDWR, 0600, 0);
 
-if(servidor == SEM_FAILED){
+    if (servidor == SEM_FAILED)
+    {
         printf("error abriendo sem servidor\n");
         exit(1);
-}
+    }
 
-//se debe validar que un cliente no se conecte si no hay servidor activo
-//falta la logica acá
-sem_post(cliente); //me conecto a server
+    sem_t *mutex = sem_open("MUTEX", O_CREAT, 0600, 1);
+	// no se pudo abrir el semaforo MUTEX
+	if (mutex == SEM_FAILED)
+	{
+		perror("error abriendo sem mutex");
+		if (errno == EEXIST)
+		{
+			printf("el semaforo MUTEX ya existe, no se puede abrir\n");
+		}
+		else if (errno == EACCES)
+		{
+			printf("el semaforo MUTEX no se puede abrir, no tengo permisos\n");
+		}
+		else
+		{
+			printf("error desconocido abriendo sem MUTEX\n");
+		}
 
-while(memoriaCompartida->intentos > 0){
-	sem_wait(servidor);//espero a que el server haya seteado todo
-
-	char palabraAEnviar[30] = "";
-	printf("[server]: %s\n",memoriaCompartida->palabraCamuflada);
-
-	fflush(stdin);
-	fflush(stdout);
-	scanf("%s",palabraAEnviar);
-	fflush(stdin);
-	fflush(stdout);
-
-	if(strcmp(palabraAEnviar,"exit")!=0){
-		memoriaCompartida->ultimaLetra = palabraAEnviar[0];
-	} else {
-		strcpy(memoriaCompartida->estadoPartida,palabraAEnviar);
-		break;
+		exit(1);
 	}
 
-	sem_post(cliente); //le aviso a server que meti una palabra
 
+    // se debe validar que un cliente no se conecte si no hay servidor activo
+    
 
+    sem_post(cliente); // me conecto a server
 
-}
+    while (memoriaCompartida->intentos > 0)
+    {
+        sem_wait(servidor); // espero a que el server haya seteado todo
 
-return 0;
+        char palabraAEnviar[30] = "";
 
+        fflush(stdin);
+        fflush(stdout);
+        
+        printf("[server]: %s\n - Intentos restantes: %d\n", memoriaCompartida->palabraCamuflada, memoriaCompartida->intentos);
+
+        fflush(stdin);
+        fflush(stdout);
+        scanf("%s", palabraAEnviar);
+        fflush(stdin);
+        fflush(stdout);
+
+        //printf("[server]: %s\n", memoriaCompartida->palabraCamuflada);
+
+        if (strcmp(palabraAEnviar, "exit") != 0)
+        {
+            memoriaCompartida->ultimaLetra = palabraAEnviar[0];
+        }
+        else
+        {
+            strcpy(memoriaCompartida->estadoPartida, palabraAEnviar);
+            sem_post(cliente); // le aviso al servidor que meti exit asi lee la rta
+            break;
+        }
+
+        sem_post(cliente); // le aviso a server que meti una palabra
+    }
+
+    return 0;
 }
--- APL2/ejercicio4/server.c
+++ APL2/ejercicio4/server.c
@@ -16,14 +16,15 @@ Integrantes del grupo:
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <time.h>
 
 #define FALSE 0
 #define TRUE 1
 #define MAX_FRASES 3
 #define MAX_LINEA 30
 
-
-typedef struct {
+typedef struct
+{
 	char nickname[30];
 	char ultimaLetra;
 	char palabra[30];
@@ -33,292 +34,411 @@ typedef struct {
 
 } SharedMemory;
 
-typedef struct {
+typedef struct
+{
 	char nickname[30];
 	int puntaje;
-	int tiempoJuego; // en segundos	
+	int tiempoJuego; // en segundos
 } Jugador;
 
+bool termProcess = FALSE;
+bool estadoPartida = FALSE;
+bool finalizarPartida = FALSE;
 
-bool termProcess = TRUE;
-
-
-void devolverPalabraJuego(char *destino, char *original) {
-    int i;
-    for (i = 0; original[i] != '\0'; i++) {
-        destino[i] = (original[i] == ' ') ? ' ' : '_';
-    }
-    destino[i] = '\0';
-}
-
-void sigusrHandler(int signal){
-	//necesito una variable para poder finalizar al servidor. completar
-	switch(signal){
-		case SIGUSR1:
-			printf("SIGUSR1 recibido, finalizando cliente...\n");
-		case SIGUSR2:
-			printf("SIGUSR2 recibido, finalizando cliente...\n");
-	}
-}
-
-//se genera un ranking de los jugadores por puntaje y por tiempo de juego mostrandolo en pantalla
-void generarRanking(Jugador *jugadores){
-	
-
-}
-void mostrarRanking(){
-	printf("Ranking de jugadores:\n");
-	//acá se debe implementar la logica para msotrar el ranking de jugadores
-}
-	
-
-
-
-
-int main(int argc, char *argv[]){
-signal(SIGINT,SIG_IGN);
-signal(SIGUSR1,sigusrHandler);
-signal(SIGUSR2,sigusrHandler);
-
-/////////////////////////////////////////////////////////////////////
-if(argc > 7) {
-        printf("parametros invalidos\n");
-        return 1;
-}
-
-char* filePath = NULL;
-
-int i = 0;
-int cantidad = 0;
-bool b_cantidad = FALSE;
-bool b_file = FALSE;
-while(i < argc)
+void devolverPalabraJuego(char *destino, char *original)
 {
-	if(argv[i] != NULL) {
-	        if(strcmp(argv[i],"-help") == 0 || strcmp(argv[i],"-h") == 0){
-			printf("Opciones :\n");
-			printf("--help o -h para mostrar la ayuda\n");
-			printf("--cantidad o -u seguido del numero maximo de usuarios que maneja el server\n");
-			printf("--archivo o -a seguido del path donde se encuentra el archivo con las palabras\n");
-			return 0;
-	        }
+	int i;
+	for (i = 0; original[i] != '\0'; i++)
+	{
+		destino[i] = (original[i] == ' ') ? ' ' : '_';
+	}
+	destino[i] = '\0';
+}
 
-		if(strcmp(argv[i],"--cantidad") == 0 || strcmp(argv[i],"-c") ==0){
-			cantidad = atoi(argv[i+1]);
-			i++;
-			b_cantidad = TRUE;
+void sigusrHandler(int signal)
+{
+	// necesito una variable para poder finalizar al servidor. completar
+	switch (signal)
+	{
+	case SIGUSR1:
+		printf("SIGUSR1 recibido, finalizando cliente...\n");
+		if(estadoPartida){
+			printf("El cliente está en una partida, no se puede finalizar ahora.\n");
+			finalizarPartida = TRUE;
+		} else {
+			printf("El cliente no está en una partida, se puede finalizar.\n");
+			finalizarPartida=TRUE;
+			termProcess = TRUE;
 		}
+	case SIGUSR2:
+		printf("SIGUSR2 recibido, finalizando cliente...\n");
+		termProcess = TRUE;
+	}
+}
 
-		if(strcmp(argv[i],"--archivo") == 0 || strcmp(argv[i],"-a") == 0) {
-			filePath = argv[i+1];
-			i++;
-			b_file = TRUE;
+// Función de comparación para qsort
+int compararJugadores(const void *a, const void *b) {
+    const Jugador *jugadorA = (const Jugador *)a;
+    const Jugador *jugadorB = (const Jugador *)b;
+    
+    // Primero comparar por puntaje (de mayor a menor)
+    if (jugadorB->puntaje != jugadorA->puntaje) {
+        return jugadorB->puntaje - jugadorA->puntaje;
+    }
+    
+    // En caso de empate, comparar por tiempo de juego (de menor a mayor)
+    return jugadorA->tiempoJuego - jugadorB->tiempoJuego;
+}
+
+void generarRanking(Jugador *jugadores, int cantidadJugadores) {
+    // Ordenar el arreglo de jugadores usando qsort
+    qsort(jugadores, cantidadJugadores, sizeof(Jugador), compararJugadores);
+}
+
+void mostrarRanking(Jugador *jugadores)
+{
+	printf("Ranking de jugadores:\n");
+	for (int i = 0; i < 50; i++)
+	{
+		if (jugadores[i].puntaje > 0)
+		{
+			printf("Jugador: %s, Puntaje: %d, Tiempo de juego: %d segundos\n",
+				   jugadores[i].nickname, jugadores[i].puntaje, jugadores[i].tiempoJuego);
 		}
 	}
-
-
-
-
-i++;
 }
 
-if(b_cantidad == FALSE){
-	printf("parametro usuarios invalido o faltante\n");
-	return 1;
-}
-if(b_file == FALSE){
-	printf("parametro archivo invalido o faltante\n");
-	return 1;
-}
+int main(int argc, char *argv[])
+{
+	signal(SIGINT, SIG_IGN);
+	signal(SIGUSR1, sigusrHandler);
+	signal(SIGUSR2, sigusrHandler);
 
-////////////////////////////////////////////////////////////////////////////
-FILE *file = fopen(filePath, "r");
-if (!file){
-	perror("Error abriendo archivo");
-	return 1;
-}
-char *frases[MAX_FRASES];
-char bufferArchivo[MAX_LINEA];
-int cantidadFrases = 0;
-
-while (fgets(bufferArchivo, MAX_LINEA, file) && cantidadFrases < MAX_FRASES){
-	// Eliminar el salto de línea final si existe
-	bufferArchivo[strcspn(bufferArchivo, "\n")] = '\0';
-	// Reservar memoria y copiar la línea
-	frases[cantidadFrases] = strdup(bufferArchivo);
-	if (!frases[cantidadFrases]){
-		perror("Fallo al reservar memoria\n");
-		fclose(file);
+	/////////////////////////////////////////////////////////////////////
+	if (argc > 7)
+	{
+		printf("parametros invalidos\n");
 		return 1;
 	}
-	cantidadFrases++;
-}
 
-fclose(file);
+	char *filePath = NULL;
 
+	int i = 0;
+	int cantidad = 0;
+	bool b_cantidad = FALSE;
+	bool b_file = FALSE;
+	while (i < argc)
+	{
+		if (argv[i] != NULL)
+		{
+			if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0)
+			{
+				printf("Opciones :\n");
+				printf("--help o -h para mostrar la ayuda\n");
+				printf("--cantidad o -u seguido del numero maximo de usuarios que maneja el server\n");
+				printf("--archivo o -a seguido del path donde se encuentra el archivo con las palabras\n");
+				return 0;
+			}
 
-////////////////////////////////////////////////////////////////////////////
+			if (strcmp(argv[i], "--cantidad") == 0 || strcmp(argv[i], "-c") == 0)
+			{
+				cantidad = atoi(argv[i + 1]);
+				i++;
+				b_cantidad = TRUE;
+			}
 
-
-
-SharedMemory* memoriaCompartida;
-
-int sharedMemInt = shm_open("SHARED_MEM", O_RDWR | O_CREAT, 0600);
-
-if(sharedMemInt == -1){
-	perror("shm_open"); // Imprime el error
-    if (errno == EACCES) {
-        printf("Error: Permisos insuficientes\n");
-    } else if (errno == ENOENT) {
-        printf("Error: Objeto no encontrado\n");
-    } else if (errno == EEXIST) {
-        printf("Error: El objeto ya existe.\n");
-    } else if (errno == EINVAL){
-        printf("Error: Nombre no válido\n");
-    }else if (errno == EMFILE || errno == ENFILE){
-        printf("Error: Demasiados descriptores o archivos abiertos.\n");
-    }
-	exit(1);
-}
-
-ftruncate(sharedMemInt,sizeof(SharedMemory));
-
-memoriaCompartida = (SharedMemory*)mmap(NULL,sizeof(SharedMemory),PROT_WRITE,MAP_SHARED,sharedMemInt,0);
-
-
-memoriaCompartida->intentos = cantidad;
-strcpy(memoriaCompartida->estadoPartida,"00000000");
-
-
-close(sharedMemInt);
-
-sem_t* mutex = sem_open("MUTEX",O_CREAT,0600,1);
-//no se pudo abrir el semaforo MUTEX
-if(mutex == SEM_FAILED){
-	perror("error abriendo sem mutex");
-	if(errno == EEXIST){
-		printf("el semaforo MUTEX ya existe, no se puede abrir\n");
-	}else if(errno== EACCES){
-		printf("el semaforo MUTEX no se puede abrir, no tengo permisos\n");
-	}else{
-		printf("error desconocido abriendo sem MUTEX\n");
-	}
-
-	exit(1);
-}
-
-
-
-
-sem_t* cliente = sem_open("CLIENTE",O_CREAT,0600,0);
-//no se pudo abrir el semaforo CLIENTE
-if(cliente == SEM_FAILED){
-	perror("error abriendo sem cliente");
-	if(errno == EEXIST){
-		printf("el semaforo CLIENTE ya existe, no se puede abrir\n");
-	}else if(errno== EACCES){
-		printf("el semaforo CLIENTE no se puede abrir, no tengo permisos\n");
-	}else{
-		printf("error desconocido abriendo sem CLIENTE\n");
-	}
-	exit(1);
-}
-
-sem_t* servidor = sem_open("SERVIDOR",O_CREAT,0600,0);
-//no se pudo abrir el semaforo SERVIDOR
-if(servidor == SEM_FAILED){
-	perror("error abriendo sem servidor");
-if(errno == EEXIST){
-	printf("el semaforo SERVIDOR ya existe, no se puede abrir\n");
-}else if(errno== EACCES){
-	printf("el semaforo SERVIDOR no se puede abrir, no tengo permisos\n");
-}else{
-	printf("error desconocido abriendo sem SERVIDOR\n");
-}
-	exit(1);
-}
-
-if(servidor == SEM_FAILED){
-	perror("error abriendo sem servidor");
-	if(errno == EEXIST){
-		printf("el semaforo SERVIDOR ya existe, no se puede abrir\n");
-	}else if(errno== EACCES){
-		printf("el semaforo SERVIDOR no se puede abrir, no tengo permisos\n");
-	}else{
-		printf("error desconocido abriendo sem SERVIDOR\n");
-	}
-	exit(1);
-}
-Jugador jugadores[50]; //arreglo de jugadores, maximo 50 jugadores
-jugadores[0].puntaje = 0; //inicializo el primer jugador con puntaje 0
-
-//lógica del servidor 
-while(1){
-	printf("esperando al cliente\n");
-	int indice = rand() % cantidadFrases;
-	strcpy(memoriaCompartida->palabra,frases[indice]);
-	sem_wait(cliente); //espero que se conecte 1 cliente
-
-	printf("entró el cliente %s\n",memoriaCompartida->nickname);
-	devolverPalabraJuego(memoriaCompartida->palabraCamuflada,memoriaCompartida->palabra);
-
-
-	int estadoPartida = FALSE;
-
-
-	TAG:
-	while(memoriaCompartida->intentos > 0){
-		sem_post(servidor); //le aviso al cliente que ya estoy
-		sem_wait(cliente); //espero que el cliente haya metido una palabra
-
-
-		if(strcmp(memoriaCompartida->estadoPartida,"exit")==0){
-			//falta hacer funcion de contabilizar puntaje (el append en el file y sarasa)
-			break;
-		}
-
-		//bandera generica
-		int acierto=0;
-
-
-		//loop para validar la letra que entro con la palabra posta y reflejar el cambio en la palabra camuflada
-		for (int i = 0; memoriaCompartida->palabra[i] != '\0';i++){
-			if(memoriaCompartida->palabra[i] == memoriaCompartida->ultimaLetra &&  memoriaCompartida->palabraCamuflada[i] != memoriaCompartida->ultimaLetra){
-				memoriaCompartida->palabraCamuflada[i] = memoriaCompartida->ultimaLetra;
-				acierto++;
+			if (strcmp(argv[i], "--archivo") == 0 || strcmp(argv[i], "-a") == 0)
+			{
+				filePath = argv[i + 1];
+				i++;
+				b_file = TRUE;
 			}
 		}
-
-		if (!acierto){
-			memoriaCompartida->intentos--;
-		}
-
-
-
-		//si el cliente termino de hacer bien la palabra
-		if (strcmp(memoriaCompartida->palabraCamuflada,memoriaCompartida->palabra) == 0){
-			//falta implementar sumar 1 al puntaje
-			
-			indice = rand() % cantidadFrases;
-		        strcpy(memoriaCompartida->palabra,frases[indice]);
-		        devolverPalabraJuego(memoriaCompartida->palabraCamuflada,memoriaCompartida->palabra);
-			goto TAG;
-		}
+		i++;
 	}
 
-	//falta resetear las variables para que se pueda conectar el proximo cliente
+	if (b_cantidad == FALSE)
+	{
+		printf("parametro usuarios invalido o faltante\n");
+		return 1;
+	}
 
-}
+	if (b_file == FALSE)
+	{
+		printf("parametro archivo invalido o faltante\n");
+		return 1;
+	}
 
-//limpiar los recursos cuando se termine el servidor
+	////////////////////////////////////////////////////////////////////////////
+	FILE *file = fopen(filePath, "r");
+	if (!file)
+	{
+		perror("Error abriendo archivo");
+		return 1;
+	}
+	char *frases[MAX_FRASES];
+	char bufferArchivo[MAX_LINEA];
+	int cantidadFrases = 0;
 
+	while (fgets(bufferArchivo, MAX_LINEA, file) && cantidadFrases < MAX_FRASES)
+	{
+		// Eliminar el salto de línea final si existe
+		bufferArchivo[strcspn(bufferArchivo, "\n")] = '\0';
+		// Reservar memoria y copiar la línea
+		frases[cantidadFrases] = strdup(bufferArchivo);
+		if (!frases[cantidadFrases])
+		{
+			perror("Fallo al reservar memoria\n");
+			fclose(file);
+			return 1;
+		}
+		cantidadFrases++;
+	}
 
+	fclose(file);
 
+	////////////////////////////////////////////////////////////////////////////
 
+	SharedMemory *memoriaCompartida;
 
+	int sharedMemInt = shm_open("SHARED_MEM", O_RDWR | O_CREAT, 0600);
 
+	if (sharedMemInt == -1)
+	{
+		perror("shm_open"); // Imprime el error
+		if (errno == EACCES)
+		{
+			printf("Error: Permisos insuficientes\n");
+		}
+		else if (errno == ENOENT)
+		{
+			printf("Error: Objeto no encontrado\n");
+		}
+		else if (errno == EEXIST)
+		{
+			printf("Error: El objeto ya existe.\n");
+		}
+		else if (errno == EINVAL)
+		{
+			printf("Error: Nombre no válido\n");
+		}
+		else if (errno == EMFILE || errno == ENFILE)
+		{
+			printf("Error: Demasiados descriptores o archivos abiertos.\n");
+		}
+		exit(1);
+	}
 
+	ftruncate(sharedMemInt, sizeof(SharedMemory));
 
+	memoriaCompartida = (SharedMemory *)mmap(NULL, sizeof(SharedMemory), PROT_WRITE, MAP_SHARED, sharedMemInt, 0);
 
-return 0;
+	memoriaCompartida->intentos = cantidad;
+	strcpy(memoriaCompartida->estadoPartida, "00000000");
+
+	close(sharedMemInt);
+
+	sem_t *mutex = sem_open("MUTEX", O_CREAT, 0600, 1);
+	// no se pudo abrir el semaforo MUTEX
+	if (mutex == SEM_FAILED)
+	{
+		perror("error abriendo sem mutex");
+		if (errno == EEXIST)
+		{
+			printf("el semaforo MUTEX ya existe, no se puede abrir\n");
+		}
+		else if (errno == EACCES)
+		{
+			printf("el semaforo MUTEX no se puede abrir, no tengo permisos\n");
+		}
+		else
+		{
+			printf("error desconocido abriendo sem MUTEX\n");
+		}
+
+		exit(1);
+	}
+
+	sem_t *cliente = sem_open("CLIENTE", O_CREAT, 0600, 0);
+	// no se pudo abrir el semaforo CLIENTE
+	if (cliente == SEM_FAILED)
+	{
+		perror("error abriendo sem cliente");
+		if (errno == EEXIST)
+		{
+			printf("el semaforo CLIENTE ya existe, no se puede abrir\n");
+		}
+		else if (errno == EACCES)
+		{
+			printf("el semaforo CLIENTE no se puede abrir, no tengo permisos\n");
+		}
+		else
+		{
+			printf("error desconocido abriendo sem CLIENTE\n");
+		}
+		exit(1);
+	}
+
+	sem_t *servidor = sem_open("SERVIDOR", O_CREAT, 0600, 0);
+	// no se pudo abrir el semaforo SERVIDOR
+	if (servidor == SEM_FAILED)
+	{
+		perror("error abriendo sem servidor");
+		if (errno == EEXIST)
+		{
+			printf("el semaforo SERVIDOR ya existe, no se puede abrir\n");
+		}
+		else if (errno == EACCES)
+		{
+			printf("el semaforo SERVIDOR no se puede abrir, no tengo permisos\n");
+		}
+		else
+		{
+			printf("error desconocido abriendo sem SERVIDOR\n");
+		}
+		exit(1);
+	}
+
+	if (servidor == SEM_FAILED)
+	{
+		perror("error abriendo sem servidor");
+		if (errno == EEXIST)
+		{
+			printf("el semaforo SERVIDOR ya existe, no se puede abrir\n");
+		}
+		else if (errno == EACCES)
+		{
+			printf("el semaforo SERVIDOR no se puede abrir, no tengo permisos\n");
+		}
+		else
+		{
+			printf("error desconocido abriendo sem SERVIDOR\n");
+		}
+		exit(1);
+	}
+	Jugador jugadores[50];	  // arreglo de jugadores, maximo 50 jugadores
+	jugadores[0].puntaje = 0; // inicializo el primer jugador con puntaje 0
+	
+	int cantJugadores = 0;
+
+	// lógica del servidor
+	while (!termProcess)
+	{
+		printf("esperando al cliente\n");
+
+		int indice = rand() % cantidadFrases;
+		strcpy(memoriaCompartida->palabra, frases[indice]);
+
+		if(!estadoPartida & finalizarPartida){
+			printf("Finalizando servidor...\n");
+			break;
+		}
+		
+		sem_wait(cliente); // espero que se conecte 1 cliente
+
+		printf("entró el cliente %s\n", memoriaCompartida->nickname);
+
+		devolverPalabraJuego(memoriaCompartida->palabraCamuflada, memoriaCompartida->palabra);
+	
+		// inicializo al jugador en 0
+		jugadores[cantJugadores].puntaje = 0;
+		jugadores[cantJugadores].tiempoJuego = 0;
+		strcpy(jugadores[cantJugadores].nickname, memoriaCompartida->nickname);
+		
+
+		estadoPartida = TRUE; // para el sigusr1
+
+		time_t tiempoInicio = time(NULL);
+
+	TAG:
+		while (memoriaCompartida->intentos > 0 & !termProcess)
+		{
+			sem_post(servidor); // le aviso al cliente que ya estoy
+			sem_wait(cliente);	// espero que el cliente haya metido una palabra
+			
+
+			if (strcmp(memoriaCompartida->estadoPartida, "exit") == 0)
+			{
+				// falta hacer funcion de contabilizar puntaje (el append en el file y sarasa)
+				time_t tiempoFin = time(NULL);
+				int tiempoJuego = difftime(tiempoFin, tiempoInicio);
+				printf("El cliente %s ha salido del juego.\n", memoriaCompartida->nickname);
+				printf("Puntaje final: %d\n", jugadores[cantJugadores].puntaje);
+				printf("Tiempo de juego: %d segundos\n", tiempoJuego);
+				jugadores[cantJugadores].tiempoJuego = tiempoJuego;
+				strcpy(memoriaCompartida->estadoPartida, "00000000");
+				
+				//sem_post(mutex);
+				break;
+			}
+
+			// bandera generica
+			int acierto = 0;
+
+			// loop para validar la letra que entro con la palabra posta y reflejar el cambio en la palabra camuflada
+			for (int i = 0; memoriaCompartida->palabra[i] != '\0'; i++)
+			{
+				if (memoriaCompartida->palabra[i] == memoriaCompartida->ultimaLetra && memoriaCompartida->palabraCamuflada[i] != memoriaCompartida->ultimaLetra)
+				{
+					memoriaCompartida->palabraCamuflada[i] = memoriaCompartida->ultimaLetra;
+					acierto++;
+				}
+			}
+
+			if (!acierto)
+			{
+				memoriaCompartida->intentos--;
+			}
+
+			// si el cliente termino de hacer bien la palabra
+			if (strcmp(memoriaCompartida->palabraCamuflada, memoriaCompartida->palabra) == 0)
+			{
+				
+				time_t tiempoFin = time(NULL);
+				int tiempoJuego = difftime(tiempoFin, tiempoInicio);
+				printf("El cliente %s ha ganado!\n", memoriaCompartida->nickname);
+				printf("Tiempo de juego: %d segundos\n", tiempoJuego);
+
+				jugadores[cantJugadores].puntaje += 1;
+				jugadores[cantJugadores].tiempoJuego = tiempoJuego;
+
+				indice = rand() % cantidadFrases;
+				strcpy(memoriaCompartida->palabra, frases[indice]);
+
+				tiempoInicio = time(NULL);
+				
+				strcpy(memoriaCompartida->estadoPartida, "00000000");
+
+				devolverPalabraJuego(memoriaCompartida->palabraCamuflada, memoriaCompartida->palabra);
+				memoriaCompartida->intentos = cantidad;
+				printf("Nueva palabra: %s\n", memoriaCompartida->palabraCamuflada);
+				printf("La palabra nueva es: %s\n", memoriaCompartida->palabra);
+				goto TAG;
+			}
+		}
+		strcpy(memoriaCompartida->estadoPartida, "00000000");
+		//sem_post(mutex);
+		cantJugadores++;
+
+		// falta resetear las variables para que se pueda conectar el proximo cliente
+		memoriaCompartida->intentos = cantidad;
+		estadoPartida = FALSE;
+	}
+
+	generarRanking(jugadores, cantJugadores);
+	mostrarRanking(jugadores);
+
+	// limpiar los recursos cuando se termine el servidor
+	sem_close(mutex);
+	sem_close(cliente);
+	sem_close(servidor);
+	sem_unlink("MUTEX");
+	sem_unlink("CLIENTE");
+	sem_unlink("SERVIDOR");
+	munmap(memoriaCompartida, sizeof(SharedMemory));
+	close(sharedMemInt);
+	shm_unlink("SHARED_MEM");
+	printf("Servidor finalizado.\n");
+
+	return 0;
 }
